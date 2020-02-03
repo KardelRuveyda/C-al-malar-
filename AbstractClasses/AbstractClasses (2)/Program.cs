001abe9ed433a0d1cf6cbfbeb6c08fe118bf90de
Kardel Rüveyda Çetin <ruveydakardelcetin@gmail.com>
2020-02-03 13:52:30 +0300

    AbstractClasses ve Constructors

---
+++ AbstractClasses/AbstractClasses (2)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interfaces+virtulamethod 
+
+            Database database = new Oracle();
+            database.Delete();
+            database.Add();
+
+            Database database2 = new Oracle();
+            database2.Add();
+            database2.Delete();
+
+            Console.ReadLine();
+          
+            //Delete abstract olarak tanımlandığı için diğer metodlarda ovverride denmiş 
+            //Add ise ortak ; o direk geliyor. 
+         
+         }
+    }
+
+    abstract class Database
+    {
+        //Ekleme heryerde aynı
+        //abstract class newlenemiyor
+        public void Add()
+        {
+            Console.WriteLine("Added by default.");
+        }
+        //Silme farklı
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database
+    {
+        //abstract içi dolu olmayan virtual metottur.
+        public override void Delete()
+        {
+            Console.WriteLine("Sql Server deleted by. ");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.Write("Oracle Deleted by.");
+        }
+    }
+    
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customer = new CustomerManager(10);
+            customer.List();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        //Constructor oluşturmak için ctor (iki kere tab ) 
+
+        private int _count;
+
+        //bana integer bir sayı ver
+        public CustomerManager(int count)
+        {
+            _count = count;
+        }
+        
+        public void List()
+        {
+            Console.WriteLine("Listed! {0} items ",_count);
+        }
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+
+    }
+}
