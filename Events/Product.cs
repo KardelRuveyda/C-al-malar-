11e0e7db5c59b8763e3974bd377d7e9a132a5d42
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-07 22:54:39 +0300

    Events

---
+++ Events/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock { get { return _stock; }
+        
+            set
+            {
+                _stock = value;
+                if(value <= 15 && StockControlEvent!=null)
+                {
+                    StockControlEvent();
+                }
+            }
+
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= _stock;
+
+            Console.WriteLine("{1} Stok az kaldı.: {0} ", Stock,ProductName);
+            Console.Read();
+        }
+    }
+}
